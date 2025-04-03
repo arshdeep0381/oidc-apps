81b056da76a0ea72748446034c5d7052e6281520
arshdeep0381 <arsh.deep.0381@gmail.com>
2025-04-03 23:00:40 +0530

    Update landing page

--- examples/vue-spa/src/views/HomeView.vue
+++ examples/vue-spa/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { useAuth } from '@/lib/auth'
 
-const { isAuthenticated, login, logout } = useAuth()
+const { isAuthenticated, user, login, logout } = useAuth()
 </script>
 
 <template>
@@ -9,27 +9,33 @@ const { isAuthenticated, login, logout } = useAuth()
     <main role="main"
       class="flex flex-col items-center justify-center h-5/6 space-y-8 text-center px-4 py-16 lg:pt-32 md:pt-16 sm:pt-8">
       <h1 class="mt-4 text-2xl font-extrabold tracking-tight text-slate-900 sm:text-5xl xl:max-w-[43.5rem]">
-        Vue.js Application Example using OpenID Connect
+        Utility Apps Platform
       </h1>
       <p class="mt-4 max-w-lg text-lg text-slate-700">
-        This example demonstrates how to authenticate users in a Vue Application using OpenID Connect Protocol.
+        Application uses OpenID Connect Protocol for authentication.
+      </p>
+      <p v-if="isAuthenticated">
+        <h3>
+          Authenticated User!
+        </h3>
+        <h4>
+        User:  {{user}}
+        </h4>
+      </p>
+      <p v-else>
+        <h3>Not Authenticated</h3>
       </p>
       <button class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700" v-if="isAuthenticated" @click="logout">
         Logout
       </button>
       <button class="inline-flex justify-center rounded-lg text-sm font-semibold py-3 px-4 bg-slate-900 text-white hover:bg-slate-700" v-else @click="login">
-        Login with TestID
+        Login with ArmureAM ID
       </button>
-      <p class="mt-4 max-w-lg text-slate-700">
-        If you want to checkout out how to implement OpenID Connect in your Vue Application, take a look at the <a
-          class="text-indigo-600"
-          href="https://github.com/cerberauth/openid-connect-examples/tree/main/examples/vue-app">source code</a>.
-      </p>
     </main>
 
     <footer class="text-center py-4">
       <p class="text-sm text-gray-500">
-        Proudly part of <a class="text-indigo-600" href="https://www.cerberauth.com">CerberAuth</a> community.
+        
       </p>
     </footer>
   </div>
