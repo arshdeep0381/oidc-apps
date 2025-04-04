0eee49c8b64828cb30c773512fcae6407972727d
arshdeep0381 <arsh.deep.0381@gmail.com>
2025-04-04 11:46:35 +0530

    Externalize OIDC config

--- examples/nextjs-app/auth.ts
+++ examples/nextjs-app/auth.ts
@@ -2,9 +2,9 @@ import NextAuth from 'next-auth'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [{
-    id: 'testid',
-    name: 'TestID',
-    issuer: 'https://testid.cerberauth.com',
+    id: 'armuream',
+    name: 'ArmureAM',
+    issuer: process.env.AUTH_ISSUER,
     type: 'oidc',
     clientId: process.env.AUTH_CLIENT_ID,
     clientSecret: process.env.AUTH_CLIENT_SECRET,
